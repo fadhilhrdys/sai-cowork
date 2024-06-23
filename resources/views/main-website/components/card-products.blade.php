4712508cf763cf3ff3e0809dfdb24c3b371ce009
fadhil <fadhilahardiyasaa@gmail.com>
2024-06-23 21:37:35 +0700

    create product card component

---
+++ resources/views/main-website/components/card-products.blade.php
@@ -0,0 +1,92 @@
+<div class="cards">
+    <div class="card">
+        <img class="thumbnail" src="{{ ('img/gallery-1.jpg') }}" alt="co working space">
+        <div class="body-card">
+            <h3>Event Space</h3>
+            <p>
+                Event space is a room that can be used for activities such as
+                corporate events, social events, private events, gatherings and so on.
+            </p>
+        </div>
+        <div class="footer-card">
+            <div class="meta-info">
+                <div class="meta">
+                    <img src="{{ ('img/icon-person.png') }}" alt="">
+                    <h6>55 Persons</h6>
+                </div>
+                <div class="meta">
+                    <img src="{{ ('img/icon-price-tag.png') }}" alt="">
+                    <h6>From Rp 1.000.000/hour</h6>
+                </div>
+            </div>
+            <button>Find Space</button>
+        </div>
+    </div>
+    <div class="card">
+        <img class="thumbnail" src="{{ ('img/gallery-2.jpg') }}" alt="co working space">
+        <div class="body-card">
+            <h3>Event Space</h3>
+            <p>
+                Event space is a room that can be
+            </p>
+        </div>
+        <div class="footer-card">
+            <div class="meta-info">
+                <div class="meta">
+                    <img src="{{ ('img/icon-person.png') }}" alt="">
+                    <h6>55 Persons</h6>
+                </div>
+                <div class="meta">
+                    <img src="{{ ('img/icon-price-tag.png') }}" alt="">
+                    <h6>From Rp 450.000/hour</h6>
+                </div>
+            </div>
+            <button>Find Space</button>
+        </div>
+    </div>
+    <div class="card">
+        <img class="thumbnail" src="{{ ('img/gallery-1.jpg') }}" alt="co working space">
+        <div class="body-card">
+            <h3>Event Space</h3>
+            <p>
+                Event space is a room that can be used for activities.
+            </p>
+        </div>
+        <div class="footer-card">
+            <div class="meta-info">
+                <div class="meta">
+                    <img src="{{ ('img/icon-person.png') }}" alt="">
+                    <h6>55 Persons</h6>
+                </div>
+                <div class="meta">
+                    <img src="{{ ('img/icon-price-tag.png') }}" alt="">
+                    <h6>From Rp 1.000.000/hour</h6>
+                </div>
+            </div>
+            <button>Find Space</button>
+        </div>
+    </div>
+    <div class="card">
+        <img class="thumbnail" src="{{ ('img/gallery-2.jpg') }}" alt="co working space">
+        <div class="body-card">
+            <h3>Event Space</h3>
+            <p>
+                Event space is a room that can be used for activities such as
+                corporate events, social events, private events, gatherings and so on.
+            </p>
+        </div>
+        <div class="footer-card">
+            <div class="meta-info">
+                <div class="meta">
+                    <img src="{{ ('img/icon-person.png') }}" alt="">
+                    <h6>55 Persons</h6>
+                </div>
+                <div class="meta">
+                    <img src="{{ ('img/icon-price-tag.png') }}" alt="">
+                    <h6>From Rp 450.000/hour</h6>
+                </div>
+            </div>
+            <button>Find Space</button>
+        </div>
+    </div>
+</div>
