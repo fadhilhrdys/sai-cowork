b8b3c6351465ba6140df5255e1c70f170c2d7138
fadhil <fadhilahardiyasaa@gmail.com>
2024-07-04 21:07:18 +0700

    fix url image

--- resources/views/main-website/components/footer.blade.php
+++ resources/views/main-website/components/footer.blade.php
@@ -1,11 +1,11 @@
 <footer>
     <div class="wrapper">
         <div class="col-left">
-            <img class="logo" src="{{ ('img/logo-black-footer.png') }}" alt="sai cowork">
+            <img class="logo" src="{{ ('/img/logo-black-footer.png') }}" alt="sai cowork">
             <div class="socials">
-                <img src="{{ ('img/icon-ig.png') }}" alt="sai cowork">
-                <img src="{{ ('img/icon-linkedin.png') }}" alt="sai cowork">
-                <img src="{{ ('img/icon-tiktok.png') }}" alt="sai cowork">
+                <img src="{{ ('/img/icon-ig.png') }}" alt="sai cowork">
+                <img src="{{ ('/img/icon-linkedin.png') }}" alt="sai cowork">
+                <img src="{{ ('/img/icon-tiktok.png') }}" alt="sai cowork">
             </div>
         </div>
         <div class="col-center">
@@ -21,14 +21,14 @@
             <div class="contacts">
                 <h4>Contact Us</h4>
                 <ul>
-                    <li><a href="#"><img src="{{ ('img/icon-email.png') }}" alt="sai cowork">info@saicowork.com</a></li>
-                    <li><a href="#"><img src="{{ ('img/icon-wa.png') }}" alt="sai cowork">+62 812 8701 8707</a></li>
+                    <li><a href="#"><img src="{{ ('/img/icon-email.png') }}" alt="sai cowork">info@saicowork.com</a></li>
+                    <li><a href="#"><img src="{{ ('/img/icon-wa.png') }}" alt="sai cowork">+62 812 8701 8707</a></li>
                 </ul>
             </div>
         </div>
         <div class="col-right">
             <h4>Download SAI Cowork App</h4>
-            <img src="{{ ('img/playstore.png') }}" alt="sai cowork">
+            <img src="{{ ('/img/playstore.png') }}" alt="sai cowork">
             <br>
             <span> &copy; Copyright 2024 <br> SAI Cowork <br> Member of SAI Coorporation</span>
         </div>
--- resources/views/main-website/components/navbar.blade.php
+++ resources/views/main-website/components/navbar.blade.php
@@ -1,14 +1,14 @@
 <div class="navigation-bar">
     <div class="wrapper-mobile">
-        <img src="{{ ('img/logo-black-header.png') }}" alt="sai cowork">
+        <img src="{{ ('/img/logo-black-header.png') }}" alt="sai cowork">
         <div class="col-right">
             <a href="http://" class="btn-login">Login</a>
-            <button class="open-sidemenu"><img src="{{ ('img/icon-sidemenu.png') }}"></button>
+            <button class="open-sidemenu"><img src="{{ ('/img/icon-sidemenu.png') }}"></button>
         </div>
     </div>
     <div class="wrapper-desktop">
         <div class="col-left">
-            <img src="{{ ('img/logo-black-header.png') }}" alt="sai cowork">
+            <img src="{{ ('/img/logo-black-header.png') }}" alt="sai cowork">
             <nav>
                 <a href="http://">Location</a>
                 <a href="http://">Gallery</a>
@@ -17,7 +17,7 @@
         </div>
         <div class="col-right">
             <a href="http://" class="btn-login">Login</a>
-            <a href="http://" class="btn-sign-up">Sign up <img src="{{ ('img/icon-sign-up.png') }}"></a>
+            <a href="http://" class="btn-sign-up">Sign up <img src="{{ ('/img/icon-sign-up.png') }}"></a>
         </div>
     </div>
 </div>
@@ -25,10 +25,10 @@
 <div class="side-menu-bar">
     <div class="wrapper">
         <div class="header">
-            <img src="{{ ('img/logo-black-header.png') }}" alt="sai cowork">
+            <img src="{{ ('/img/logo-black-header.png') }}" alt="sai cowork">
             <div class="col-right">
                 <a href="http://" class="btn-login">Login</a>
-                <button class="close-sidemenu"><img src="{{ ('img/icon-close.png') }}"></button>
+                <button class="close-sidemenu"><img src="{{ ('/img/icon-close.png') }}"></button>
             </div>
         </div>
         <nav>
@@ -36,7 +36,7 @@
             <a href="http://">Gallery</a>
             <a href="http://">Blog</a>
             <hr>
-            <a href="http://" class="btn-sign-up">Sign up <img src="{{ ('img/icon-sign-up.png') }}"></a>
+            <a href="http://" class="btn-sign-up">Sign up <img src="{{ ('/img/icon-sign-up.png') }}"></a>
         </nav>
     </div>
 </div>
